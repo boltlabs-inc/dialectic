6b36fc5102d3bc09693ab8fd743247da1ea3b2f3
Kenny Foner <kwf@very.science>
2020-12-08 16:29:30 -0500

    Move call_by into separate package

--- src/backend.rs
+++ src/backend.rs
@@ -11,8 +11,6 @@ use async_trait::async_trait;
 pub use call_by::*;
 use std::future::Future;
 
-pub mod call_by;
-
 /// If something is `Transmit<'a, T, Convention>`, we can use it to [`Transmit::send`] a message of
 /// type `T` by [`Val`], [`Ref`], or [`Mut`], depending on the calling convention specified.
 pub trait Transmit<'a, T, Convention: CallingConvention>
