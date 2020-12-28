016f6ac6f9383a6c57b7e84351fe1ad35235a599
Kenny Foner <kwf@very.science>
2020-12-28 17:53:32 -0500

    Update tally example to use `Break`

--- examples/tally/client.rs
+++ examples/tally/client.rs
@@ -1,93 +1,116 @@
-use dialectic::backend::serde::format::length_delimited_bincode;
+use std::fmt::Display;
+
+use dialectic::backend::serde::{
+    format::{length_delimited_bincode, Bincode},
+    SymmetricalError,
+};
 use dialectic::constants::*;
 use dialectic::*;
 
-use tokio::io::{self, AsyncBufReadExt, BufReader};
+use tokio::io::{self, AsyncBufRead, AsyncBufReadExt, BufReader, Lines};
 use tokio::net::TcpStream;
 
 mod server;
-use server::{get_port, Operation, Server};
+use server::{get_port, wrap_socket, BincodeTcpChan, Operation, Server};
+use tokio_util::codec::LengthDelimitedCodec;
 
 // The client is the dual of the server
 type Client = <Server as Session>::Dual;
+
+/// Loop presenting a prompt until the user enters a parseable string or the input ends, returning
+/// `Ok(None)` on end of input and `Err(_)` on other errors.
+async fn parse_line_loop<T, E, R>(
+    prompt: &str,
+    lines: &mut Lines<R>,
+    mut parse: impl FnMut(&str) -> Result<T, E>,
+) -> io::Result<Option<T>>
+where
+    R: AsyncBufRead + Unpin,
+{
+    loop {
+        eprint!("{}", prompt);
+        match lines.next_line().await? {
+            Some(line) => match parse(line.trim()) {
+                Ok(t) => break Ok(Some(t)),
+                Err(_) => continue,
+            },
+            None => break Ok(None),
+        }
+    }
+}
+
+/// Parse a string as a yes-or-no response
+fn yes_or_no(s: &str) -> Result<bool, ()> {
+    match s.to_uppercase().as_ref() {
+        "Y" | "YES" => Ok(true),
+        "N" | "NO" => Ok(false),
+        _ => Err(()),
+    }
+}
+
+fn number_or_tally(s: &str) -> Result<Option<i64>, ()> {
+    let s = s.trim();
+    if s == "" {
+        Ok(None)
+    } else if let Ok(n) = s.parse() {
+        Ok(Some(n))
+    } else {
+        Err(())
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Connect to the server and assemble into a transport using length-delimited bincode
     let port = get_port()?;
-    let mut connection = TcpStream::connect(("127.0.0.1", port)).await?;
-    let (rx, tx) = connection.split();
-    let (tx, rx) = length_delimited_bincode(tx, rx, 3, 8 * 1024);
+    let connection = TcpStream::connect(("127.0.0.1", port)).await?;
+    interact(wrap_socket::<Client>(connection, 8 * 1024)).await?;
+    Ok(())
+}
 
+async fn interact(
+    mut chan_outer: BincodeTcpChan<Client>,
+) -> Result<(), Box<dyn std::error::Error>> {
     // Hook up stdin as a stream of lines, and stdout as a writer
     let mut stdin = BufReader::new(io::stdin()).lines();
 
-    // Now let's use session types!
-    let mut chan_outer = Client::wrap(tx, rx);
-    let chan_outer = 'outer: loop {
+    'outer: loop {
         // Get the desired operation from the user
-        let operation = loop {
-            eprint!("Operation (+/*): ");
-            match stdin.next_line().await? {
-                Some(l) if l.trim() == "+" => break Operation::Sum,
-                Some(l) if l.trim() == "*" => break Operation::Product,
-                Some(_) => continue,
-                None => {
-                    // If the user ends the input stream here, before selecting an operation, there
-                    // is no possible way (aside from inventing an operation from thin air) to
-                    // continue to follow the protocol and make it to the end. We choose to
-                    // ungracefully end the program immediately, triggering an error on the server
-                    // side.
-                    std::process::exit(1)
-                }
-            };
-        };
+        let operation = parse_line_loop("Operation (+ / *): ", &mut stdin, str::parse)
+            .await?
+            .unwrap_or_else(|| std::process::exit(1));
 
         // Send the desired operation
         let mut chan_inner = chan_outer.send(&operation).await?;
 
-        eprintln!("Input numbers to tally (tally with `=`):");
+        eprintln!("Input numbers to tally (newline to tally):");
         chan_outer = 'inner: loop {
-            let n = loop {
-                let line = stdin.next_line().await?;
-                if let Some(line) = line {
-                    if line == "=" {
-                        // User wants to get the final tally
-                        let (tally, chan_inner) = chan_inner.choose(_1).await?.recv().await?;
-                        println!("{}", tally);
-                        loop {
-                            // Ask whether to do another tally, and jump to the right place
-                            eprintln!("Another tally? (Y/n)");
-                            match stdin.next_line().await?.map(|s| s.to_uppercase()) {
-                                Some(l) if l == "Y" || l == "YES" => {
-                                    break 'inner chan_inner.choose(_1).await?
-                                }
-                                Some(l) if l == "N" || l == "NO" => {
-                                    break 'outer chan_inner.choose(_0).await?
-                                }
-                                None => break 'outer chan_inner.choose(_0).await?,
-                                _ => continue,
-                            }
-                        }
-                    } else {
-                        // User wants to submit another number to the tally
-                        match line.parse() {
-                            Ok(n) => break n,
-                            Err(err) => eprintln!("Parse error: {}", err),
-                        }
+            match parse_line_loop(&format!("{} ", operation), &mut stdin, number_or_tally).await? {
+                Some(Some(n)) => {
+                    // Choose to repeat the inner loop, and repeat
+                    chan_inner = chan_inner.choose(_0).await?.send(&n).await?;
+                }
+                Some(None) => {
+                    // User wants to get the final tally
+                    let (tally, chan_inner) = chan_inner.choose(_1).await?.recv().await?;
+                    println!("= {}", tally);
+                    match parse_line_loop("Another tally? (Y/n): ", &mut stdin, yes_or_no)
+                        .await?
+                        .unwrap_or(false)
+                    {
+                        true => break 'inner chan_inner.choose(_1).await?,
+                        false => break 'outer chan_inner.choose(_0).await?,
                     }
-                } else {
+                }
+                None => {
                     // End of input stream, so let's cleanly finish up with the server
                     let (tally, chan_inner) = chan_inner.choose(_1).await?.recv().await?;
-                    eprintln!("=");
-                    println!("{}", tally);
+                    println!("= {}", tally);
                     break 'outer chan_inner.choose(_0).await?;
                 }
             };
-            // Choose to repeat the inner loop, and repeat
-            chan_inner = chan_inner.choose(_0).await?.send(&n).await?;
         };
-    };
-    chan_outer.close();
+    }
+    .close();
     Ok(())
 }
--- examples/tally/server.rs
+++ examples/tally/server.rs
@@ -1,15 +1,28 @@
 #![allow(unused)]
-use dialectic::backend::serde::format::{length_delimited_bincode, Bincode};
+use std::{
+    fmt::{self, Display},
+    str::FromStr,
+};
+use thiserror::Error;
+
+use dialectic::backend::serde::{
+    format::{length_delimited_bincode, Bincode},
+    Receiver, Sender, SymmetricalError,
+};
 use dialectic::constants::*;
 use dialectic::unary::types::*;
 use dialectic::*;
 
 use serde_crate::{Deserialize, Serialize};
-use tokio::net::{TcpListener, TcpStream};
+use tokio::net::{
+    tcp::ReadHalf,
+    tcp::{OwnedReadHalf, OwnedWriteHalf, WriteHalf},
+    TcpListener, TcpStream,
+};
 use tokio_util::codec::LengthDelimitedCodec;
 
 pub type Server =
-    Loop<Recv<Operation, Loop<Offer<(Recv<i64, Recur>, Send<i64, Offer<(End, Recur<_1>)>>)>>>>;
+    Loop<Recv<Operation, Loop<Offer<(Recv<i64>, Send<i64, Offer<(Break<_1>, Break)>>)>>>>;
 
 #[derive(Debug, Copy, Clone, Serialize, Deserialize)]
 #[serde(crate = "serde_crate")] // we only need this because we renamed serde in Cargo.toml
@@ -19,19 +32,70 @@ pub enum Operation {
 }
 
 impl Operation {
-    fn unit(&self) -> i64 {
+    pub fn unit(&self) -> i64 {
         match self {
             Operation::Sum => 0,
             Operation::Product => 1,
         }
     }
 
-    fn combine(&self, x: i64, y: i64) -> i64 {
+    pub fn combine(&self, x: i64, y: i64) -> i64 {
         match self {
             Operation::Sum => x + y,
             Operation::Product => x * y,
         }
     }
+}
+
+#[derive(Debug, Copy, Clone, Error)]
+#[error("couldn't parse operation")]
+pub struct ParseOperationError;
+
+impl FromStr for Operation {
+    type Err = ParseOperationError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "+" => Ok(Operation::Sum),
+            "*" => Ok(Operation::Product),
+            _ => Err(ParseOperationError),
+        }
+    }
+}
+
+impl Display for Operation {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Operation::Sum => write!(f, "+"),
+            Operation::Product => write!(f, "*"),
+        }
+    }
+}
+
+// TODO: generalize this into the library to make a normalized Chan type synonym so things work out
+// nicely instead of weirdly clashing because of auto-ff-ing through loops and recurs?
+#[allow(type_alias_bounds)]
+pub type BincodeTcpChan<P, E = ()>
+where
+    P: Actionable<E>,
+    E: Environment,
+= Chan<
+    Sender<Bincode, LengthDelimitedCodec, OwnedWriteHalf>,
+    Receiver<Bincode, LengthDelimitedCodec, OwnedReadHalf>,
+    P::Action,
+    P::Env,
+>;
+
+pub fn wrap_socket<'a, P>(mut socket: TcpStream, max_length: usize) -> BincodeTcpChan<P>
+where
+    P: NewSession,
+    P::Dual: Actionable<()>,
+    <P::Env as EachSession>::Dual: Environment,
+    <<P::Dual as Actionable<()>>::Env as EachSession>::Dual: Environment,
+{
+    let (rx, tx) = socket.into_split();
+    let (tx, rx) = length_delimited_bincode(tx, rx, 4, max_length);
+    P::wrap(tx, rx)
 }
 
 /// Try to parse a port number as the first argument to the program, returning a descriptive error
@@ -60,7 +124,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         let (mut socket, addr) = listener.accept().await?;
         eprintln!("Accepted: {}", addr);
         tokio::spawn(async move {
-            serve(socket)
+            serve(wrap_socket::<Server>(socket, 8 * 1024))
                 .await
                 .map(|_| eprintln!("Finished: {}", addr))
                 .map_err(|err| eprintln!("Error on: {}: {}", addr, err))
@@ -69,16 +133,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 async fn serve(
-    mut socket: TcpStream,
-) -> Result<(), backend::serde::Error<Bincode, Bincode, LengthDelimitedCodec, LengthDelimitedCodec>>
-{
-    // Assemble a length-delimited bincode transport...
-    let (rx, tx) = socket.split();
-    let (tx, rx) = length_delimited_bincode(tx, rx, 3, 8 * 1024);
-
-    // Now let's use session types!
-    let mut chan_outer = Server::wrap(tx, rx);
-    let chan_outer = 'outer: loop {
+    mut chan_outer: BincodeTcpChan<Server>,
+) -> Result<(), SymmetricalError<Bincode, LengthDelimitedCodec>> {
+    'outer: loop {
         // What operation should we use?
         let (op, mut chan_inner) = chan_outer.recv().await?;
 
@@ -106,9 +163,7 @@ async fn serve(
                 }
             });
         };
-    };
-
-    // Prove to the type system that we completed the protocol
-    chan_outer.close();
+    }
+    .close();
     Ok(())
 }
