86f51cc2eb5a47ace4404920d7fd7de4928610b6
Sean Leffler <sean@errno.com>
2021-02-17 15:53:01 -0800

    I like curly braces.

--- dialectic/examples/tally.rs
+++ dialectic/examples/tally.rs
@@ -17,7 +17,7 @@ async fn main() {
 
 /// The session from the client's perspective.
 // pub type Client = Loop<Choose<(Send<Operation, Call<ClientTally, Continue>>, Done)>>;
-pub type Client = session!(
+pub type Client = session! {
     loop {
         choose {
             _0 => {
@@ -27,7 +27,7 @@ pub type Client = session!(
             _1 => break,
         }
     }
-);
+};
 
 /// The implementation of the client.
 async fn client(
