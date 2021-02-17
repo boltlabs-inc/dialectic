69145a31b7f4c3333c89106da4025047d8c01c96
Sean Leffler <sean@errno.com>
2021-02-17 13:57:16 -0800

    dialectic-macro wrapper crate

---
+++ dialectic-macro/src/lib.rs
@@ -0,0 +1,14 @@
+extern crate proc_macro;
+
+use {dialectic_compiler::Soup, proc_macro::TokenStream, syn::parse_macro_input};
+
+#[proc_macro]
+pub fn session(input: TokenStream) -> TokenStream {
+    let ast = parse_macro_input!(input as dialectic_compiler::Ast);
+    let mut soup = Soup::new();
+    let cfg = soup.to_cfg(&ast);
+    let session = soup[cfg].to_session(&soup, &mut Vec::new());
+    syn::parse_str::<proc_macro2::TokenStream>(&session.to_string())
+        .expect("bad output from dialectic-compiler!")
+        .into()
+}
