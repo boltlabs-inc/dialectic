0e5c897a0ddf3ed7a77c5c2d374665526e34abc9
Sean Leffler <sean@errno.com>
2021-02-17 15:19:40 -0800

    Simplify interface and reduce possibility of forgetting elimination pass

--- dialectic-macro/src/lib.rs
+++ dialectic-macro/src/lib.rs
@@ -1,15 +1,11 @@
 extern crate proc_macro;
 
-use {dialectic_compiler::Soup, proc_macro::TokenStream, syn::parse_macro_input};
+use {proc_macro::TokenStream, syn::parse_macro_input};
 
 #[proc_macro]
 pub fn session(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as dialectic_compiler::Ast);
-    let mut soup = Soup::new();
-    let cfg = soup.to_cfg(&ast);
-    soup.eliminate_breaks_and_labels(&mut Vec::new(), cfg);
-    let session = soup[cfg].to_session(&soup, &mut Vec::new());
-    syn::parse_str::<proc_macro2::TokenStream>(&session.to_string())
+    syn::parse_str::<proc_macro2::TokenStream>(&ast.to_session().to_string())
         .expect("bad output from dialectic-compiler!")
         .into()
 }
