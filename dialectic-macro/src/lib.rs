afd16d333ff368469145c6a5a6fcf572d67ebf47
Sean Leffler <sean@errno.com>
2021-02-18 11:29:17 -0800

    Propagate span info to get type errors to be shown correctly

--- dialectic-macro/src/lib.rs
+++ dialectic-macro/src/lib.rs
@@ -1,12 +1,13 @@
 extern crate proc_macro;
 
-use {proc_macro::TokenStream, syn::parse_macro_input};
+use {proc_macro::TokenStream, quote::ToTokens, syn::parse_macro_input};
 
 #[proc_macro]
 #[allow(non_snake_case)]
 pub fn Session(input: TokenStream) -> TokenStream {
-    let invocation = parse_macro_input!(input as dialectic_compiler::Invocation);
-    syn::parse_str::<proc_macro2::TokenStream>(&invocation.ast.to_session().to_string())
-        .expect("bad output from dialectic-compiler!")
+    parse_macro_input!(input as dialectic_compiler::Invocation)
+        .ast
+        .to_session()
+        .to_token_stream()
         .into()
 }
