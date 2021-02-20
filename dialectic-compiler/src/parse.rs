3fb05f0a37b0cf87e05f9267b2b5c2240a45b519
Sean Leffler <sean@errno.com>
2021-02-19 17:20:07 -0800

    First pass at vastly improved error messages (needs cleanup)

--- dialectic-compiler/src/parse.rs
+++ dialectic-compiler/src/parse.rs
@@ -3,11 +3,23 @@ use {
     syn::{
         braced,
         parse::{Error, Parse, ParseStream, Result},
+        spanned::Spanned,
         token, Ident, Lifetime, Token, Type,
     },
 };
 
-use crate::{Invocation, Syntax};
+use crate::{Invocation, Syntax, SyntaxNode};
+
+trait JoinSpansExt {
+    fn combine<T: Spanned>(&mut self, new: T) -> T;
+}
+
+impl JoinSpansExt for Span {
+    fn combine<T: Spanned>(&mut self, new: T) -> T {
+        *self = self.join(new.span()).unwrap_or(*self);
+        new
+    }
+}
 
 mod kw {
     syn::custom_keyword!(recv);
@@ -26,7 +38,7 @@ enum Direction {
 
 struct SplitArm {
     dir: Direction,
-    arm: Syntax,
+    arm: SyntaxNode,
 }
 
 impl Parse for SplitArm {
@@ -41,14 +53,14 @@ impl Parse for SplitArm {
         } else {
             return Err(lookahead.error());
         };
-        let arm = input.parse::<Syntax>()?;
+        let arm = input.parse::<SyntaxNode>()?;
         Ok(SplitArm { dir, arm })
     }
 }
 
 struct ChoiceArm {
     index: usize,
-    arm: Syntax,
+    arm: SyntaxNode,
     span: Span,
 }
 
@@ -63,45 +75,76 @@ impl Parse for ChoiceArm {
             .and_then(|s| s.parse::<usize>().map_err(|e| input.error(e)))?;
         input.parse::<Token![=>]>()?;
         span = span.join(input.span()).unwrap_or(span);
-        let arm = input.parse::<Syntax>()?;
+        let arm = input.parse::<SyntaxNode>()?;
         Ok(ChoiceArm { index, arm, span })
     }
 }
 
-impl Parse for Syntax {
+struct Block(SyntaxNode);
+
+impl Parse for Block {
+    fn parse(input: ParseStream) -> Result<Self> {
+        // Ast::Block: { <Ast>; <Ast>; ... }
+        let block_span = input.span();
+        let content;
+        braced!(content in input);
+        let nodes = content
+            .parse_terminated::<SyntaxNode, Token![;]>(SyntaxNode::parse)?
+            .into_iter()
+            .collect();
+
+        Ok(Block(SyntaxNode {
+            expr: Syntax::Block(nodes),
+            span: block_span,
+        }))
+    }
+}
+
+impl Parse for SyntaxNode {
     fn parse(input: ParseStream) -> Result<Self> {
         let lookahead = input.lookahead1();
         if lookahead.peek(kw::recv) {
             // Ast::Recv: recv <type>
-            input.parse::<kw::recv>()?;
-            Ok(Syntax::Recv(input.parse::<Type>()?))
+            let recv_span = input.parse::<kw::recv>()?.span();
+            Ok(SyntaxNode {
+                expr: Syntax::Recv(input.parse::<Type>()?),
+                span: recv_span,
+            })
         } else if lookahead.peek(kw::send) {
             // Ast::Send: send <type>
-            input.parse::<kw::send>()?;
-            Ok(Syntax::Send(input.parse::<Type>()?))
+            let send_span = input.parse::<kw::send>()?.span();
+            Ok(SyntaxNode {
+                expr: Syntax::Send(input.parse::<Type>()?),
+                span: send_span,
+            })
         } else if lookahead.peek(kw::call) {
             // Ast::Call: call <type> or call <block>
-            input.parse::<kw::call>()?;
+            let call_span = input.parse::<kw::call>()?.span();
             let lookahead = input.lookahead1();
 
-            if lookahead.peek(token::Brace) {
-                let content;
-                braced!(content in input);
-                let nodes = content
-                    .parse_terminated::<Syntax, Token![;]>(Syntax::parse)?
-                    .into_iter()
-                    .collect();
-                Ok(Syntax::Call(Box::new(Syntax::Block(nodes))))
+            let callee = if lookahead.peek(token::Brace) {
+                input.parse::<Block>()?.0
             } else {
                 let ty = input.parse::<Type>().map_err(|mut e| {
                     e.combine(lookahead.error());
                     e
                 })?;
-                Ok(Syntax::call(Syntax::Type(ty)))
-            }
+
+                SyntaxNode {
+                    span: ty.span(),
+                    expr: Syntax::Type(ty),
+                }
+            };
+
+            Ok(SyntaxNode {
+                expr: Syntax::Call(Box::new(callee)),
+                span: call_span,
+            })
         } else if lookahead.peek(kw::choose) {
             // Ast::Choose: choose { _0 => <Ast>, _1 => <Ast>, ... }
-            input.parse::<kw::choose>()?;
+            let kw_span = input.parse::<kw::choose>()?.span();
+            let choose_span = kw_span.join(input.span()).unwrap_or(kw_span);
+
             let content;
             braced!(content in input);
             let choice_arms = content.parse_terminated::<ChoiceArm, Token![,]>(ChoiceArm::parse)?;
@@ -121,10 +164,15 @@ impl Parse for Syntax {
                 arm_asts.push(choice_arm.arm);
             }
 
-            Ok(Syntax::Choose(arm_asts))
+            Ok(SyntaxNode {
+                expr: Syntax::Choose(arm_asts),
+                span: choose_span,
+            })
         } else if lookahead.peek(kw::offer) {
             // Ast::Offer: offer { _0 => <Ast>, _1 => <Ast>, ... }
-            input.parse::<kw::offer>()?;
+            let kw_span = input.parse::<kw::offer>()?.span();
+            let offer_span = kw_span.join(input.span()).unwrap_or(kw_span);
+
             let content;
             braced!(content in input);
             let choice_arms = content.parse_terminated::<ChoiceArm, Token![,]>(ChoiceArm::parse)?;
@@ -144,9 +192,14 @@ impl Parse for Syntax {
                 arm_asts.push(choice_arm.arm);
             }
 
-            Ok(Syntax::Offer(arm_asts))
+            Ok(SyntaxNode {
+                expr: Syntax::Offer(arm_asts),
+                span: offer_span,
+            })
         } else if lookahead.peek(kw::split) {
-            input.parse::<kw::split>()?;
+            let kw_span = input.parse::<kw::split>()?.span();
+            let split_span = kw_span.join(input.span()).unwrap_or(kw_span);
+
             let content;
             braced!(content in input);
             let mut split_arms = content
@@ -164,55 +217,66 @@ impl Parse for Syntax {
                 split_arms.swap(0, 1);
             }
 
-            Ok(Syntax::Split(
-                Box::new(split_arms[0].arm.clone()),
-                Box::new(split_arms[1].arm.clone()),
-            ))
+            Ok(SyntaxNode {
+                expr: Syntax::Split(
+                    Box::new(split_arms[0].arm.clone()),
+                    Box::new(split_arms[1].arm.clone()),
+                ),
+                span: split_span,
+            })
         } else if lookahead.peek(Token![loop]) || lookahead.peek(Lifetime) {
             // Ast::Loop: 'label loop { ... }
+            let mut loop_span;
             let label = if input.peek(Lifetime) {
-                let name = input.parse::<Lifetime>()?.ident.to_string();
-                input.parse::<Token![:]>()?;
+                loop_span = input.span();
+                let lifetime = loop_span.combine(input.parse::<Lifetime>()?);
+                let name = lifetime.ident.to_string();
+                loop_span.combine(input.parse::<Token![:]>()?);
+                loop_span.combine(input.parse::<Token![loop]>()?);
                 Some(name)
+            } else {
+                loop_span = input.parse::<Token![loop]>()?.span();
+                None
+            };
+
+            let Block(block) = input.parse::<Block>()?;
+            Ok(SyntaxNode {
+                expr: Syntax::Loop(label, Box::new(block)),
+                span: loop_span,
+            })
+        } else if lookahead.peek(Token![break]) {
+            // Ast::Break: break 'label
+            let mut break_span = input.parse::<Token![break]>()?.span();
+            let label = if input.peek(Lifetime) {
+                let lifetime = input.parse::<Lifetime>()?;
+                break_span.combine(&lifetime);
+                Some(lifetime.ident.to_string())
             } else {
                 None
             };
 
-            input.parse::<Token![loop]>()?;
-            let content;
-            braced!(content in input);
-            let nodes = content
-                .parse_terminated::<Syntax, Token![;]>(Syntax::parse)?
-                .into_iter()
-                .collect();
-            Ok(Syntax::Loop(label, Box::new(Syntax::Block(nodes))))
-        } else if lookahead.peek(Token![break]) {
-            // Ast::Break: break 'label
-            input.parse::<Token![break]>()?;
-            let label = if input.peek(Lifetime) {
-                Some(input.parse::<Lifetime>()?.ident.to_string())
-            } else {
-                None
-            };
-            Ok(Syntax::Break(label))
+            Ok(SyntaxNode {
+                expr: Syntax::Break(label),
+                span: break_span,
+            })
         } else if lookahead.peek(Token![continue]) {
             // Ast::Continue: continue 'label
-            input.parse::<Token![continue]>()?;
+            let mut continue_span = input.parse::<Token![continue]>()?.span();
             let label = if input.peek(Lifetime) {
-                Some(input.parse::<Lifetime>()?.ident.to_string())
+                let lifetime = input.parse::<Lifetime>()?;
+                continue_span.combine(&lifetime);
+                Some(lifetime.ident.to_string())
             } else {
                 None
             };
-            Ok(Syntax::Continue(label))
+
+            Ok(SyntaxNode {
+                expr: Syntax::Continue(label),
+                span: continue_span,
+            })
         } else if lookahead.peek(token::Brace) {
             // Ast::Block: { <Ast>; <Ast>; ... }
-            let content;
-            braced!(content in input);
-            let nodes = content
-                .parse_terminated::<Syntax, Token![;]>(Syntax::parse)?
-                .into_iter()
-                .collect();
-            Ok(Syntax::Block(nodes))
+            Ok(input.parse::<Block>()?.0)
         } else {
             // Attempt to parse as a direct type Ast::Type: <type>
             // Otherwise, fail and report all other errors from the lookahead.
@@ -225,7 +289,11 @@ impl Parse for Syntax {
                     return Err(combined);
                 }
             };
-            Ok(Syntax::Type(ty))
+
+            Ok(SyntaxNode {
+                span: ty.span(),
+                expr: Syntax::Type(ty),
+            })
         }
     }
 }
@@ -233,10 +301,13 @@ impl Parse for Syntax {
 impl Parse for Invocation {
     fn parse(input: ParseStream) -> Result<Self> {
         let nodes = input
-            .parse_terminated::<Syntax, Token![;]>(Syntax::parse)?
+            .parse_terminated::<SyntaxNode, Token![;]>(SyntaxNode::parse)?
             .into_iter()
             .collect::<Vec<_>>();
-        let ast = Syntax::Block(nodes);
+        let ast = SyntaxNode {
+            expr: Syntax::Block(nodes),
+            span: Span::call_site(),
+        };
         Ok(Invocation { syntax: ast })
     }
 }
--- dialectic-macro/src/lib.rs
+++ dialectic-macro/src/lib.rs
@@ -5,9 +5,12 @@ use {proc_macro::TokenStream, quote::ToTokens, syn::parse_macro_input};
 #[proc_macro]
 #[allow(non_snake_case)]
 pub fn Session(input: TokenStream) -> TokenStream {
-    parse_macro_input!(input as dialectic_compiler::Invocation)
+    let result = parse_macro_input!(input as dialectic_compiler::Invocation)
         .syntax
-        .to_session()
-        .to_token_stream()
-        .into()
+        .to_session();
+
+    match result {
+        Ok(compiled) => compiled.to_token_stream().into(),
+        Err(error) => error.to_compile_error().into(),
+    }
 }
