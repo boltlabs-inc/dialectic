cb871204909517be60e46d832019b71325d41afd
Sean Leffler <sean@errno.com>
2021-02-23 11:57:22 -0800

    More pairing (refactoring, adding documentation, and satisfying lints)

--- dialectic-compiler/src/lib.rs
+++ dialectic-compiler/src/lib.rs
@@ -1,3 +1,12 @@
+#![warn(missing_docs)]
+#![warn(missing_copy_implementations, missing_debug_implementations)]
+#![warn(unused_qualifications, unused_results)]
+#![warn(future_incompatible)]
+#![warn(unused)]
+#![forbid(broken_intra_doc_links)]
+
+//! This is a crate. FIXME.
+
 use {proc_macro2::Span, thiserror::Error};
 
 mod cfg;
