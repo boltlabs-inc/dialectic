e936ef361f22a5fc832eb49345304bfb37e17bd2
Shea Leffler <sean@errno.com>
2021-03-03 10:47:21 -0800

    More internal documentation for cfg.rs, flow.rs, and target.rs.

--- dialectic-compiler/src/flow.rs
+++ dialectic-compiler/src/flow.rs
@@ -5,6 +5,9 @@ use {
 
 use crate::cfg::{Cfg, Ir};
 
+/// The output of a flow analysis is two sets: the "passable" set, indicating which nodes have a
+/// possible path "through" them to their continuation - and the "haltable" set, indicating which
+/// nodes can possibly be run all the way to `Done`.
 #[derive(Debug, Clone)]
 pub struct FlowAnalysis {
     pub passable: HashSet<Index>,
@@ -17,14 +20,23 @@ impl FlowAnalysis {
     }
 }
 
+/// Analyze a CFG for passability of every contained node.
 pub fn analyze(cfg: &Cfg) -> FlowAnalysis {
     Solver::new(cfg).solve()
 }
 
+/// Flow analysis works by using a constraint solver: we try to solve for whether every node in the
+/// graph is "passable", and in the process of doing so, we also solve "haltable" and "breaks from
+/// within some node or its children to some given parent loop node" constraints.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum Constraint {
+    /// A `Passable` constraint carries the information of which node we are trying to check is
+    /// passable or not.
     Passable(Index),
+    /// A `Haltable` constraint for checking whether or not a node is "haltable".
     Haltable(Index),
+    /// A `BreakableTo` constraint for checking whether or not a loop can be "broken to" by a node
+    /// within its body.
     BreakableTo {
         breaks_from: Index,
         breaks_to: Index,
@@ -95,9 +107,11 @@ pub struct Solver<'a> {
     passable: HashSet<Index>,
     /// The set of nodes which are currently proven to satisfy the `Haltable` constraint.
     haltable: HashSet<Index>,
-    /// The set of pairs of nodes which are currently proven to satisfy the `BreakableTo` constraint, in the order of `breaks_from` followed by `breaks_to`.
+    /// The set of pairs of nodes which are currently proven to satisfy the `BreakableTo`
+    /// constraint, in the order of `breaks_from` followed by `breaks_to`.
     breakable_to: HashSet<(Index, Index)>,
-    /// The number of implications examined since the last progress was made. (If this exceeds the length of the implication queue, the solver cannot make any more progress.)
+    /// The number of implications examined since the last progress was made. (If this exceeds the
+    /// length of the implication queue, the solver cannot make any more progress.)
     progress: usize,
     /// The queue of [`Implication`]s yet to be proven.
     queue: VecDeque<Implication>,
@@ -247,22 +261,37 @@ fn preconditions(cfg: &Cfg, constraint: Constraint) -> Dnf {
     /// Compute the preconditions for a `Passable` constraint.
     fn passable_preconditions(cfg: &Cfg, node: Index) -> Dnf {
         match &cfg[node].expr {
+            // Trivially, `Send`, `Recv`, and `Call(None)` nodes are passable. We also assume that
+            // for any `Type` node, the external type it references is passable, and that for any
+            // `Error` node, assuming it passable will give the most possible relevant errors
+            // reported.
             Ir::Send(_) | Ir::Recv(_) | Ir::Type(_) | Ir::Error | Ir::Call(None) => {
                 Dnf::trivially_true()
             }
-            Ir::Loop(None) | Ir::Break(_) | Ir::Continue(_) => Dnf::trivially_false(),
+            // Trivially, `Break` and `Continue` nodes are never passable, because they always jump
+            // and never call their continuations.
+            Ir::Break(_) | Ir::Continue(_) => Dnf::trivially_false(),
+            // `Call` nodes are only passable if their continuations are haltable.
             Ir::Call(Some(callee)) => Dnf::only_if(vec![Constraint::Haltable(*callee)]),
+            // Similarly to call, split nodes are only passable if both arms are haltable.
             Ir::Split(tx_only, rx_only) => {
                 let arms = tx_only.iter().chain(rx_only);
                 Dnf::only_if(arms.map(|&arm| Constraint::Haltable(arm)).collect())
             }
+            // `Offer` and `Choose` nodes are passable if any arm is passable; however, the
+            // passability of an `Offer` or `Choose` typically does not matter because as control
+            // flow analysis must be run after scope resolution, they should not have an implicit
+            // continuation.
             Ir::Offer(choices) | Ir::Choose(choices) => Dnf(choices
                 .iter()
                 .filter_map(Option::as_ref)
                 .map(|&c| vec![Constraint::Passable(c)])
                 .collect()),
-            Ir::Loop(Some(body)) => Dnf::only_if(vec![Constraint::BreakableTo {
-                breaks_from: *body,
+            // The `Loop` case is the most complex case here. A `Loop` only has its continuation
+            // called when a `Break` occurs targeting the loop in question, so it is only passable
+            // if it is `BreakableTo` from some node inside it.
+            Ir::Loop(body) => Dnf::only_if(vec![Constraint::BreakableTo {
+                breaks_from: body.expect("loop bodies should always be `Some`"),
                 breaks_to: node,
             }]),
         }
@@ -272,23 +301,34 @@ fn preconditions(cfg: &Cfg, constraint: Constraint) -> Dnf {
     fn haltable_preconditions(cfg: &Cfg, node_index: Index) -> Dnf {
         let node = &cfg[node_index];
         match &node.expr {
+            // Trivially, `Send`, `Recv`, and `Type` are only haltable if they continue to a
+            // haltable node (as they always continue.) We also assume `Error` behaves this way to
+            // try to get the maximum possible number of reported relevant errors.
             Ir::Send(_) | Ir::Recv(_) | Ir::Type(_) | Ir::Error => match node.next {
                 Some(cont) => Dnf::only_if(vec![Constraint::Haltable(cont)]),
                 None => Dnf::trivially_true(),
             },
+            // Nodes are always haltable if they are passable and their continuation halts. Instead
+            // of spitting out constraints on the child nodes of these cases, we rely on their
+            // definition of passability. Note that if the continuation is `None`, then the node
+            // halts only if it is passable, so in that case a `Haltable` constraint on it is not
+            // generated.
             Ir::Call(_) | Ir::Split(_, _) | Ir::Loop(_) => {
                 let mut conj = vec![Constraint::Passable(node_index)];
                 conj.extend(node.next.map(Constraint::Haltable));
                 Dnf::only_if(conj)
             }
+            // `Choose`/`Offer` are haltable only if any of their choices are haltable.
             Ir::Choose(choices) | Ir::Offer(choices) => Dnf(choices
                 .iter()
                 .filter_map(Option::as_ref)
                 .chain(node.next.as_ref())
                 .map(|&c| vec![Constraint::Haltable(c)])
                 .collect()),
-
+            // A `Continue` only halts if whatever it jumps to halts.
             Ir::Continue(of_loop) => Dnf::only_if(vec![Constraint::Haltable(*of_loop)]),
+            // Similarly, a `Break` is a kind of jump to the continuation of its relevant loop, and
+            // halts only if the continuation of the loop halts.
             Ir::Break(of_loop) => match cfg[*of_loop].next {
                 Some(cont) => Dnf::only_if(vec![Constraint::Haltable(cont)]),
                 None => Dnf::trivially_true(),
@@ -299,7 +339,11 @@ fn preconditions(cfg: &Cfg, constraint: Constraint) -> Dnf {
     /// Compute the preconditions for a `BreakableTo` constraint.
     fn breakable_to_preconditions(cfg: &Cfg, breaks_from: Index, breaks_to: Index) -> Dnf {
         let node = &cfg[breaks_from];
+        // For most nodes, `BreakableTo` is inductive on its continuation.
         match &node.expr {
+            // Trivially, `Send`, `Recv`, and `Type` will only break to a loop if their continuation
+            // does... as obviously they do not break. Though its behavior is undefined, we assume
+            // `Error` also behaves this way.
             Ir::Send(_) | Ir::Recv(_) | Ir::Type(_) | Ir::Error => match node.next {
                 Some(cont) => Dnf::only_if(vec![Constraint::BreakableTo {
                     breaks_from: cont,
@@ -307,6 +351,10 @@ fn preconditions(cfg: &Cfg, constraint: Constraint) -> Dnf {
                 }]),
                 None => Dnf::trivially_false(),
             },
+            // `Call` and `Split` may break from within, but that doesn't actually count as them
+            // being breakable to a loop. Instead, similarly to `Send`, `Recv`, and `Type`, they are
+            // `BreakableTo` only if their continuation is, but we must also know whether they are
+            // passable (so that we can know if control flow can reach that break if it is present.)
             Ir::Call(_) | Ir::Split(_, _) => {
                 let mut conj = vec![Constraint::Passable(breaks_from)];
                 conj.extend(node.next.map(|cont| Constraint::BreakableTo {
@@ -315,6 +363,7 @@ fn preconditions(cfg: &Cfg, constraint: Constraint) -> Dnf {
                 }));
                 Dnf::only_if(conj)
             }
+            // `Choose` and `Offer` break only if an arm breaks.
             Ir::Choose(choices) | Ir::Offer(choices) => Dnf(choices
                 .iter()
                 .filter_map(Option::as_ref)
@@ -326,15 +375,15 @@ fn preconditions(cfg: &Cfg, constraint: Constraint) -> Dnf {
                     }]
                 })
                 .collect()),
+            // A loop only breaks to another loop in one of two cases: 1.) it contains a break
+            // targeting the loop in question. 2.) it is passable, and once it is passed, its
+            // continuation may break to the target.
             Ir::Loop(body) => {
-                let mut disj = vec![];
-
-                if let Some(body) = body {
-                    disj.push(vec![Constraint::BreakableTo {
-                        breaks_from: *body,
-                        breaks_to,
-                    }]);
-                }
+                let mut disj = vec![vec![Constraint::BreakableTo {
+                    breaks_from: body
+                        .expect("loop bodies should be nonempty after scope resolution"),
+                    breaks_to,
+                }]];
 
                 if let Some(cont) = node.next {
                     disj.push(vec![
@@ -348,7 +397,10 @@ fn preconditions(cfg: &Cfg, constraint: Constraint) -> Dnf {
 
                 Dnf(disj)
             }
+            // If we find a break here to the target loop, we've solved this constraint.
             Ir::Break(of_loop) if *of_loop == breaks_to => Dnf::trivially_true(),
+            // Any other breaks can be ignored. `Continue` nodes may also be ignored, as they simply
+            // repeat nodes we've already seen.
             Ir::Break(_) | Ir::Continue(_) => Dnf::trivially_false(),
         }
     }
