52e783f123a7d7c3b7624edecb5046477311fe1d
Kenny Foner <kwf@very.science>
2021-03-10 14:58:40 -0500

    Modular micro-benchmark suite which can be adapted to multiple backends

---
+++ dialectic/benches/micro.rs
@@ -0,0 +1,213 @@
+use criterion::{
+    async_executor::AsyncExecutor, criterion_group, criterion_main, measurement::WallTime, Bencher,
+    BenchmarkGroup, Criterion,
+};
+use dialectic::backend::{mpsc, null};
+use dialectic::prelude::*;
+use futures::Future;
+use std::{convert::TryInto, fmt::Debug, marker, sync::Arc, time::Instant};
+use tokio::runtime::Runtime;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+enum Primitive {
+    Send,
+    Recv,
+    Choose,
+    Offer,
+    Call,
+    Split,
+}
+
+async fn send<Tx, Rx>(
+    chan: Chan<Session! { loop { send () } }, Tx, Rx>,
+) -> Chan<Session! { loop { send () } }, Tx, Rx>
+where
+    Tx: Transmit<(), Val> + marker::Send,
+    Tx::Error: Debug,
+    Rx: marker::Send,
+{
+    chan.send(()).await.unwrap()
+}
+
+async fn recv<Tx, Rx>(
+    chan: Chan<Session! { loop { recv () } }, Tx, Rx>,
+) -> Chan<Session! { loop { recv () } }, Tx, Rx>
+where
+    Tx: marker::Send,
+    Rx: Receive<()> + marker::Send,
+    Rx::Error: Debug,
+{
+    chan.recv().await.unwrap().1
+}
+
+async fn choose<Tx, Rx>(
+    chan: Chan<Session! { loop { choose { _0 => continue } } }, Tx, Rx>,
+) -> Chan<Session! { loop { choose { _0 => continue } } }, Tx, Rx>
+where
+    Tx: Transmit<Choice<_1>, Val> + marker::Send,
+    Tx::Error: Debug,
+    Rx: marker::Send,
+{
+    chan.choose(_0).await.unwrap()
+}
+
+async fn offer<Tx, Rx>(
+    chan: Chan<Session! { loop { offer { _0 => continue } } }, Tx, Rx>,
+) -> Chan<Session! { loop { offer { _0 => continue } } }, Tx, Rx>
+where
+    Tx: marker::Send,
+    Rx: Receive<Choice<_1>> + marker::Send,
+    Rx::Error: Debug,
+{
+    offer!(chan => {
+        _0 => chan,
+    })
+    .unwrap()
+}
+
+async fn call<Tx, Rx>(
+    chan: Chan<Session! { loop { call {} } }, Tx, Rx>,
+) -> Chan<Session! { loop { call {} } }, Tx, Rx>
+where
+    Tx: marker::Send,
+    Rx: marker::Send,
+{
+    chan.call(|_| async { Ok::<_, ()>(()) })
+        .await
+        .unwrap()
+        .1
+        .unwrap()
+}
+
+async fn split<Tx, Rx>(
+    chan: Chan<Session! { loop { split { -> {}, <- {} } } }, Tx, Rx>,
+) -> Chan<Session! { loop { split { -> {} <- {} } } }, Tx, Rx>
+where
+    Tx: marker::Send,
+    Rx: marker::Send,
+{
+    chan.split(|_, _| async { Ok::<_, ()>(()) })
+        .await
+        .unwrap()
+        .1
+        .unwrap()
+}
+
+fn bench_chan_loop<S, Tx, Rx, F, Fut, N, H, A>(
+    b: &mut Bencher,
+    rt: Arc<A>,
+    channel: N,
+    primitive: Primitive,
+    f: F,
+) where
+    F: Fn(Chan<S, Tx, Rx>) -> Fut,
+    Fut: Future<Output = Chan<S, Tx, Rx>>,
+    N: Fn(Primitive, u64) -> (Tx, Rx, H),
+    S: Session,
+    Tx: marker::Send + 'static,
+    Rx: marker::Send + 'static,
+    A: AsyncExecutor,
+{
+    b.iter_custom(|iters| {
+        let (tx, rx, drop_after_bench) = channel(primitive, iters);
+        let mut chan = S::wrap(tx, rx);
+        let elapsed = rt.block_on(async {
+            let start = Instant::now();
+            for _ in 0..iters {
+                chan = f(chan).await;
+            }
+            start.elapsed()
+        });
+        drop(drop_after_bench);
+        elapsed
+    });
+}
+
+fn bench_chan_loop_group<S, Tx, Rx, Fut, H, A>(
+    g: &mut BenchmarkGroup<WallTime>,
+    rt: Arc<A>,
+    channel: fn(Primitive, u64) -> (Tx, Rx, H),
+    name: &str,
+    primitive: Primitive,
+    f: fn(Chan<S, Tx, Rx>) -> Fut,
+) where
+    Fut: Future<Output = Chan<S, Tx, Rx>>,
+    S: Session,
+    Tx: marker::Send + 'static,
+    Rx: marker::Send + 'static,
+    A: AsyncExecutor,
+{
+    g.bench_function(name, move |b| {
+        bench_chan_loop(b, rt.clone(), channel, primitive, f)
+    });
+}
+
+fn bench_all_on<Tx, Rx, H, A>(
+    c: &mut Criterion,
+    rt_name: &str,
+    rt: Arc<A>,
+    backend_name: &str,
+    channel: fn(Primitive, u64) -> (Tx, Rx, H),
+) where
+    Tx: Transmit<(), Val> + Transmit<Choice<_1>, Val> + marker::Send + 'static,
+    <Tx as Transmit<(), Val>>::Error: Debug,
+    <Tx as Transmit<Choice<_1>, Val>>::Error: Debug,
+    Rx: Receive<()> + Receive<Choice<_1>> + marker::Send + 'static,
+    <Rx as Receive<()>>::Error: Debug,
+    <Rx as Receive<Choice<_1>>>::Error: Debug,
+    A: AsyncExecutor,
+{
+    use Primitive::*;
+    let group_name = format!("{}/{}", rt_name, backend_name);
+    let mut g = c.benchmark_group(&group_name);
+    bench_chan_loop_group(&mut g, rt.clone(), channel, "send", Send, send);
+    bench_chan_loop_group(&mut g, rt.clone(), channel, "recv", Recv, recv);
+    bench_chan_loop_group(&mut g, rt.clone(), channel, "choose", Choose, choose);
+    bench_chan_loop_group(&mut g, rt.clone(), channel, "offer", Offer, offer);
+    bench_chan_loop_group(&mut g, rt.clone(), channel, "call", Call, call);
+    bench_chan_loop_group(&mut g, rt, channel, "split", Split, split);
+    g.finish();
+}
+
+fn bench_tokio_null(c: &mut Criterion) {
+    bench_all_on(
+        c,
+        "tokio",
+        Arc::new(Runtime::new().unwrap()),
+        "null",
+        |_primitive, _iters| (null::Sender::default(), null::Receiver::default(), ()),
+    )
+}
+
+fn bench_tokio_mpsc(c: &mut Criterion) {
+    use Primitive::*;
+    bench_all_on(
+        c,
+        "tokio",
+        Arc::new(Runtime::new().unwrap()),
+        "mpsc",
+        |primitive, iters| {
+            let (tx0, rx0) = mpsc::unbounded_channel();
+            let (tx1, rx1) = mpsc::unbounded_channel();
+            // Pre-allocate responses for those operations which need responses
+            match primitive {
+                Send | Choose | Call | Split => {}
+                Recv => {
+                    for _ in 0..iters {
+                        tx1.send(Box::new(())).unwrap();
+                    }
+                }
+                Offer => {
+                    for _ in 0..iters {
+                        let zero_choice: Choice<_1> = 0u8.try_into().unwrap();
+                        tx1.send(Box::new(zero_choice)).unwrap();
+                    }
+                }
+            };
+            (tx0, rx1, (tx1, rx0))
+        },
+    )
+}
+
+criterion_group!(benches, bench_tokio_null, bench_tokio_mpsc);
+criterion_main!(benches);
