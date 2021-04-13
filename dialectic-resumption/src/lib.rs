33d238ff486af76e48bfe0df9e19bc3e205f7fb7
Kenny Foner <kwf@very.science>
2021-04-13 14:11:03 -0400

    Use async trait and parameterize by persistence mechanism

--- dialectic-resumption/src/lib.rs
+++ dialectic-resumption/src/lib.rs
@@ -1,10 +1,11 @@
+use async_trait::async_trait;
 use std::{future::Future, pin::Pin};
 
 use dialectic::prelude::*;
 use dialectic::SessionIncomplete;
 
-pub type Step<Tx, Rx, State, T, E> =
-    Pin<Box<dyn Future<Output = Result<Next<Tx, Rx, State, T, E>, E>> + Send>>;
+// pub type Step<Tx, Rx, State, T, E> =
+//     Pin<Box<dyn Future<Output = Result<Next<Tx, Rx, State, T, E>, E>> + Send>>;
 
 pub struct Next<Tx, Rx, State, T, E> {
     inner: NextInner<Tx, Rx, State, T, E>,
@@ -14,7 +15,7 @@ enum NextInner<Tx, Rx, State, T, E> {
     Done(T),
     Then {
         state: State,
-        next: Step<Tx, Rx, State, T, E>,
+        next: Pin<Box<dyn Future<Output = Result<Next<Tx, Rx, State, T, E>, E>> + Send>>,
     },
 }
 
@@ -22,7 +23,7 @@ impl<Tx, Rx, State, T, E> Next<Tx, Rx, State, T, E>
 where
     Tx: Send + 'static,
     Rx: Send + 'static,
-    State: Send,
+    State: Send + 'static,
     T: 'static,
     E: 'static,
 {
@@ -32,20 +33,21 @@ where
         }
     }
 
-    pub fn then<K>(resumption: K, chan: Chan<K::Session, Tx, Rx>) -> Next<Tx, Rx, State, T, E>
+    pub fn then<S: 'static>(step: S, chan: Chan<S::Session, Tx, Rx>) -> Next<Tx, Rx, State, T, E>
     where
-        K: Resumption<Tx, Rx, Output = T, Error = E> + Into<State>,
+        S: Step<Tx, Rx, Output = T, Error = E> + Into<State>,
     {
         Next {
             inner: NextInner::Then {
-                state: resumption.clone().into(),
-                next: resumption.step(chan),
+                state: step.clone().into(),
+                next: step.step(chan),
             },
         }
     }
 }
 
-pub trait Resumption<Tx, Rx>: Clone
+#[async_trait]
+pub trait Step<Tx, Rx>: Clone
 where
     Tx: Send + 'static,
     Rx: Send + 'static,
@@ -54,84 +56,122 @@ where
     type Output;
     type Error;
 
-    fn step<State: Send>(
+    async fn step<State: Send + 'static>(
         self,
         chan: Chan<Self::Session, Tx, Rx>,
-    ) -> Step<Tx, Rx, State, Self::Output, Self::Error>
+    ) -> Result<Next<Tx, Rx, State, Self::Output, Self::Error>, Self::Error>
     where
         Self: Into<State>;
 }
 
-#[derive(Clone)]
-struct Loop;
+// #[async_trait]
+// pub trait Start<Tx, Rx>
+// where
+//     Tx: Send + 'static,
+//     Rx: Send + 'static,
+// {
+//     type Output;
+//     type Error;
 
-#[Transmitter(Tx move for ())]
-#[Receiver(Rx)]
-impl<Tx, Rx> Resumption<Tx, Rx> for Loop
-where
-    Tx::Error: std::error::Error,
-    Rx::Error: std::error::Error,
-{
-    type Session = Session! { loop { send () } };
-    type Output = ();
-    type Error = Box<dyn std::error::Error>;
+//     async fn start<State: Send + 'static>(
+//         self,
+//         tx: Tx,
+//         rx: Rx,
+//     ) -> Result<Next<Tx, Rx, State,
+//     where
+//         Self: Into<State>;
+// }
 
-    fn step<State: Send>(
-        self,
-        chan: Chan<Self::Session, Tx, Rx>,
-    ) -> Step<Tx, Rx, State, Self::Output, Self::Error>
-    where
-        Self: Into<State>,
-    {
-        Box::pin(async move {
-            let chan = chan.send(()).await?;
-            Ok(Next::then(Loop, chan))
-        })
-    }
+#[async_trait]
+pub trait Persist<Key, State> {
+    type Error;
+
+    async fn load(&self, key: &Key) -> Result<State, Self::Error>;
+
+    async fn save(&mut self, key: &Key, state: State) -> Result<(), Self::Error>;
 }
 
-// TODO: use async-trait to make this easier? no lifetime problems here, because everything is by
-// move, not by reference!
+#[async_trait]
+pub trait Handshake<Tx: Send + 'static, Rx: Send + 'static, Key>
+where
+    Self::Session: Session,
+{
+    type Session;
+    type Error;
+
+    async fn handshake(chan: Chan<Self::Session, Tx, Rx>) -> Result<Key, Self::Error>;
+}
+
+// #[derive(Clone)]
+// struct Loop;
+
+// #[Transmitter(Tx for ())]
+// #[Receiver(Rx)]
+// #[async_trait]
+// impl<Tx, Rx> Step<Tx, Rx> for Loop
+// where
+//     Tx::Error: std::error::Error,
+//     Rx::Error: std::error::Error,
+// {
+//     type Session = Session! { loop { send () } };
+//     type Output = ();
+//     type Error = Box<dyn std::error::Error>;
+
+//     async fn step<State: Send + 'static>(
+//         self,
+//         chan: Chan<Self::Session, Tx, Rx>,
+//     ) -> Result<Next<Tx, Rx, State, Self::Output, Self::Error>, Self::Error>
+//     where
+//         Self: Into<State>,
+//     {
+//         Ok(Next::then(Loop, chan.send(()).await?))
+//     }
+// }
 
 #[derive(Debug)]
-pub struct Results<Tx, Rx, State, T, E> {
+pub struct Results<Tx, Rx, T, E> {
     pub result: Result<T, E>,
     pub channels: Result<(Tx, Rx), SessionIncomplete<Tx, Rx>>,
-    pub last_state: State,
 }
 
-pub async fn resume<K, Tx, Rx, State>(
-    resumption: K,
-    tx: Tx,
-    rx: Rx,
-) -> Results<Tx, Rx, State, K::Output, K::Error>
+pub async fn handshake<H, Tx, Rx, Key>(tx: Tx, rx: Rx) -> Results<Tx, Rx, Key, H::Error>
 where
-    K: Resumption<Tx, Rx> + Into<State>,
     Tx: Send + 'static,
     Rx: Send + 'static,
-    State: Send,
+    H: Handshake<Tx, Rx, Key>,
 {
-    let ((last_state, result), channels) =
-        <K::Session as Session>::over(tx, rx, |chan| async move {
-            let mut snapshot = resumption.clone().into();
-            let mut step = resumption.step(chan);
-            loop {
-                match step.await {
-                    Ok(Next { inner }) => match inner {
-                        NextInner::Done(t) => break (snapshot, Ok(t)),
-                        NextInner::Then { next, state } => {
-                            snapshot = state;
-                            step = next;
-                        }
-                    },
-                    Err(e) => break (snapshot, Err(e)),
-                }
+}
+
+pub async fn resume<P, Key, S, Tx, Rx, State>(
+    persistence: &mut P,
+    key: &Key,
+    step: S,
+    tx: Tx,
+    rx: Rx,
+) -> Results<Tx, Rx, S::Output, S::Error>
+where
+    S: Step<Tx, Rx> + Into<State>,
+    P: Persist<Key, State>,
+    Tx: Send + 'static,
+    Rx: Send + 'static,
+    State: Send + 'static,
+    <S as Step<Tx, Rx>>::Error: From<<P as Persist<Key, State>>::Error>,
+{
+    let (result, channels) = <S::Session as Session>::over(tx, rx, |chan| async move {
+        let mut step = step.step(chan);
+        loop {
+            match step.await {
+                Ok(Next { inner }) => match inner {
+                    NextInner::Done(t) => break Ok(t),
+                    NextInner::Then { next, state } => {
+                        persistence.save(key, state).await?;
+                        step = next;
+                    }
+                },
+                Err(e) => break Err(e),
             }
-        })
-        .await;
-    Results {
-        result,
-        channels,
-        last_state,
-    }
+        }
+    })
+    .await;
+    Results { result, channels }
 }
