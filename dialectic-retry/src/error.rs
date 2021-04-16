8abe83ef8c438aa584ab241d6e95fdda2a655323
Kenny Foner <kwf@very.science>
2021-04-16 17:33:10 -0400

    Use a macro to share retry functionality
    This gets around the limitation in the borrow checker which prevents us from expressing the
    higher-order function we need to compactify this functionality.

---
+++ dialectic-retry/src/error.rs
@@ -0,0 +1,90 @@
+use crate::Handshake;
+use dialectic::{prelude::*, IncompleteHalf, SessionIncomplete};
+use tokio::sync::mpsc::error::TrySendError;
+
+pub enum ResumeError<H, Key, Tx, Rx>
+where
+    H: Handshake<Key, Tx, Rx>,
+    Tx: Send + 'static,
+    Rx: Send + 'static,
+{
+    HandshakeIncomplete(SessionIncomplete<Tx, Rx>),
+    HandshakeError {
+        error: H::Error,
+        tx: Result<Tx, IncompleteHalf<Tx>>,
+        rx: Result<Rx, IncompleteHalf<Rx>>,
+    },
+    NoSuchKey {
+        key: Key,
+        tx: Tx,
+        rx: Rx,
+    },
+    KeyAlreadyExists {
+        key: Key,
+        tx: Tx,
+        rx: Rx,
+    },
+    ResumeIncomplete(ResumeIncomplete<Tx, Rx>),
+}
+
+#[Transmitter(Tx)]
+#[Receiver(Rx)]
+impl<R, Key, Tx, Rx> ResumeError<R, Key, Tx, Rx>
+where
+    R: Handshake<Key, Tx, Rx>,
+{
+    pub fn into_halves(self) -> (Option<Tx>, Option<Rx>) {
+        use ResumeError::*;
+
+        fn unwrap_incomplete<T>(result: Result<T, IncompleteHalf<T>>) -> Option<T> {
+            match result {
+                Ok(t) => Some(t),
+                Err(IncompleteHalf::Unfinished(t)) => Some(t),
+                Err(IncompleteHalf::Unclosed) => None,
+            }
+        }
+
+        match self {
+            HandshakeIncomplete(incomplete) => {
+                let (tx, rx) = incomplete.into_halves();
+                (unwrap_incomplete(tx), unwrap_incomplete(rx))
+            }
+            HandshakeError { tx, rx, .. } => (unwrap_incomplete(tx), unwrap_incomplete(rx)),
+            NoSuchKey { tx, rx, .. } => (Some(tx), Some(rx)),
+            KeyAlreadyExists { tx, rx, .. } => (Some(tx), Some(rx)),
+            ResumeIncomplete(incomplete) => incomplete.into_halves(),
+        }
+    }
+}
+
+pub enum ResumeIncomplete<Tx, Rx> {
+    BothHalves {
+        tx_error: TrySendError<Tx>,
+        rx_error: TrySendError<Rx>,
+    },
+    TxHalf {
+        tx_error: TrySendError<Tx>,
+    },
+    RxHalf {
+        rx_error: TrySendError<Rx>,
+    },
+}
+
+impl<Tx, Rx> ResumeIncomplete<Tx, Rx> {
+    pub fn into_halves(self) -> (Option<Tx>, Option<Rx>) {
+        use ResumeIncomplete::*;
+
+        fn unwrap<T>(e: TrySendError<T>) -> T {
+            match e {
+                TrySendError::Full(t) => t,
+                TrySendError::Closed(t) => t,
+            }
+        }
+
+        match self {
+            BothHalves { tx_error, rx_error } => (Some(unwrap(tx_error)), Some(unwrap(rx_error))),
+            TxHalf { tx_error } => (Some(unwrap(tx_error)), None),
+            RxHalf { rx_error } => (None, Some(unwrap(rx_error))),
+        }
+    }
+}
--- dialectic-retry/src/lib.rs
+++ dialectic-retry/src/lib.rs
@@ -1,5 +1,32 @@
-use async_trait::async_trait;
-use dialectic::prelude::*;
-
+pub mod error;
 pub mod resume;
 pub mod retry;
+
+use dialectic::prelude::*;
+use std::time::Duration;
+
+#[async_trait::async_trait]
+pub trait Handshake<Key, Tx, Rx>
+where
+    Tx: Send + 'static,
+    Rx: Send + 'static,
+{
+    type Session: Session;
+    type Error;
+
+    async fn handshake(
+        &self,
+        chan: Chan<Self::Session, Tx, Rx>,
+    ) -> Result<(ConnectKind, Key), Self::Error>;
+}
+
+pub enum ConnectKind {
+    New,
+    Existing,
+}
+
+pub enum ErrorStrategy {
+    Retry(Duration),
+    RetryAfterReconnect,
+    Fail,
+}
