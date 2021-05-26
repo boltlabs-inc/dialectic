0d545aceb4a204b4d412e8fcf877ff2a41e35c3b
Kenny Foner <kwf@very.science>
2021-05-26 17:31:14 -0400

    Make `Backoff` serde-compatible with an optional feature

--- dialectic-reconnect/src/backoff.rs
+++ dialectic-reconnect/src/backoff.rs
@@ -1,15 +1,58 @@
 use std::time::Duration;
 
+#[cfg(feature = "serde")]
+use serde::{Deserialize, Serialize};
+
 /// A description of a backoff strategy with optional exponential delay, random jitter, maximum
 /// delay, and maximum retries. This can be used to generate retry strategies for building
 /// [`Connector`](crate::retry::Connector)s.
 #[derive(Debug, Clone, Copy)]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+#[cfg_attr(feature = "serde", serde(deny_unknown_fields))]
 pub struct Backoff {
+    #[cfg_attr(
+        all(feature = "serde", feature = "humantime_serde"),
+        serde(with = "humantime_serde")
+    )]
     initial_delay: Duration,
-    factor: f64,
+    #[cfg_attr(feature = "serde", serde(default = "defaults::exponent"))]
+    exponent: f64,
+    #[cfg_attr(feature = "serde", serde(default = "defaults::jitter"))]
+    #[cfg_attr(
+        all(feature = "serde", feature = "humantime_serde"),
+        serde(with = "humantime_serde")
+    )]
     jitter: Duration,
+    #[cfg_attr(feature = "serde", serde(default = "defaults::max_delay"))]
+    #[cfg_attr(
+        all(feature = "serde", feature = "humantime_serde"),
+        serde(with = "humantime_serde")
+    )]
     max_delay: Option<Duration>,
+    #[cfg_attr(feature = "serde", serde(default = "defaults::max_retries"))]
     max_retries: usize,
+}
+
+/// The default settings for the optional parameters, defined here as functions so they can be used
+/// in the serde implementation as well as the builder.
+mod defaults {
+    use std::time::Duration;
+
+    pub const fn exponent() -> f64 {
+        1.0
+    }
+
+    pub const fn jitter() -> Duration {
+        Duration::from_secs(0)
+    }
+
+    pub const fn max_delay() -> Option<Duration> {
+        None
+    }
+
+    pub const fn max_retries() -> usize {
+        usize::MAX
+    }
 }
 
 impl Backoff {
@@ -18,17 +61,17 @@ impl Backoff {
     pub fn with_delay(initial_delay: Duration) -> Self {
         Backoff {
             initial_delay,
-            factor: 1.0,
-            jitter: Duration::from_millis(0),
-            max_delay: None,
-            max_retries: usize::MAX,
+            exponent: defaults::exponent(),
+            jitter: defaults::jitter(),
+            max_delay: defaults::max_delay(),
+            max_retries: defaults::max_retries(),
         }
     }
 
     /// Add an exponential factor to a [`Backoff`], so that every time it is invoked, it delays for
     /// that multiple of its previous delay time.
     pub fn exponential(&mut self, factor: f64) -> &mut Self {
-        self.factor = factor;
+        self.exponent = factor;
         self
     }
 
@@ -67,7 +110,7 @@ impl Backoff {
         Strategy: Default + Sync + Send + 'static,
     {
         let initial_delay = self.initial_delay;
-        let factor = self.factor;
+        let factor = self.exponent;
         let max_retries = self.max_retries;
         let jitter = self.jitter;
         let max_delay = self.max_delay;
